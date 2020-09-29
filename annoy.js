ea96a6a82cf1d8bd69010962f7bb2a7e04dac846
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2020-09-29 12:35:49 +0100

    Add files via upload

---
+++ annoy.js
@@ -0,0 +1,18 @@
+var answer = prompt("are we there yet?");
+
+while(answer !== "yes" && answer !=="yeah"){
+	var answer = prompt("are we there yet?");
+
+}
+
+alert("yay we made it")
+
+var answer = prompt("are we there yet?");
+
+while(answer.indexof("yes") == -1){
+	var answer = prompt("are we there yet?");
+
+}
+
+alert("yay we made it")
+
